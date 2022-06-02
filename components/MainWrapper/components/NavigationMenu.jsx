8596b8386ca420909607577814cabc08c38821df
sven412TOY <popovrp@ru.itstep.org>
2022-06-02 21:19:40 +0300

    message

--- components/MainWrapper/components/NavigationMenu.jsx
+++ components/MainWrapper/components/NavigationMenu.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {Menu} from "antd";
 import {PieChartOutlined, RiseOutlined} from "@ant-design/icons";
-import {Link} from "react-router-dom";
+import Link from 'next/link'
 
 export const NavigationMenu = () => {
     return (
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item icon={<PieChartOutlined/>}>
-                <Link to={'/'}>
+                <Link href={'/'}>
                     Главная
                 </Link>
             </Menu.Item>
             <Menu.Item icon={<RiseOutlined/>}>
-                <Link to={'/coputers'}>
+                <Link href={'market/computers'}>
                     Компы
                 </Link>
             </Menu.Item>
